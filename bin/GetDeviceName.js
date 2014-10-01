3e1e8a82db0c8af21f58cc6e5130b1f09127b552
dkjha52 <dkjha52@hotmail.com>
2014-10-01 15:05:25 +0530

    Deleted Class files

--- bin/GetDeviceName.js
+++
@@ -1,7 +0,0 @@
-var iosDevice = require('/usr/local/lib/node_modules/node-ios-device');
-iosDevice.devices(function (err, devices) {
-//console.log(devices);
-devices.forEach(function (device) {
-console.log(device.name);
-});
-});
